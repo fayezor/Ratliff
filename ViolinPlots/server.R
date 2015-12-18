2175592091ee4b345ae3b4a73899dcf4e652ae1e
fayezor <faye.h.zheng@gmail.com>
2015-12-18 15:13:59 -0500

    Cleaned up formatting, added perc zeros

--- ViolinPlots/server.R
+++ ViolinPlots/server.R
@@ -2,43 +2,99 @@ library("shiny")
 library("vioplot")
 library("ggplot2")
 
-# Get dataset
+#ENSG00000000457
+#ENSG00000142515
+
+# Get dataset and groups
 data <- read.table("data/prostatedata.txt")
 groups <- ifelse(substr(names(data),1,1)=="C","Control","Knockdown")
 
-# Define server logic required to generate and plot a violin plot
+# Custom input validation 
+inputValidation <- function(input){
+  # If no gene entered
+  if (input == "e.g. ENSG..."){
+    "Please enter a gene ID"
+  }
+  # If input ID is not in dataset
+  else if (!(input %in% rownames(data))){
+    paste("'",input, "'", " is not contained in the dataset.", sep="")
+  }
+  else{ NULL }
+}
+
 shinyServer(function(input, output){
   
-  # Expression that generates a violin plot. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-  
-  output$violinPlot <- reactivePlot(function(){
+  ### Expression that generates a violin plot
+  output$violinPlot <- renderPlot(function(){
+    
+    # Get gene ID's reactively
+    geneID <- reactive({
+      # Input validation 
+      validate(
+        inputValidation(input$geneID)
+      )
+      input$geneID
+    })
     
     # Get the input gene reactively
-    geneID <- input$geneID
+    gene.df <- data.frame(gene=as.vector(as.numeric(data[rownames(data)==geneID(),])), group=groups)
+    ylab <- "Expression"
     
+    # If log transform box is selected...
     if(input$log==TRUE){
-      gene <- as.vector(as.numeric(data[rownames(data)==geneID,]))
-      gene.df <- data.frame(gene=log2(gene+1), group=groups) 
+      gene.df$gene <- log2(gene.df$gene + 1)
       ylab <- "log2( Expression )"
     }
-    else{
-      gene.df <- data.frame(gene=as.vector(as.numeric(data[rownames(data)==geneID,])), group=groups) 
-      ylab <- "Expression"
-    }
     
+    # Plotting
     p <- ggplot(gene.df, aes(y=gene, x=group, color=group)) + 
       geom_violin() + 
       geom_jitter(shape=16, position=position_jitter(0.1), cex=1.2) +
-      #theme(legend.position="none") +
-      scale_x_discrete(name=geneID) +
+      scale_x_discrete(name="") +
       scale_y_continuous(name=ylab) +
-      ggtitle("Violin Plots of Gene Expression") +
-      theme(plot.title = element_text(size=12, face="bold", vjust=2))
+      ggtitle(paste("Gene: ", input$geneID)) +
+      theme(plot.title = element_text(size=18, face="bold", vjust=2),
+            axis.text.x = element_text(colour="grey20",size=18),
+            legend.position="none")
     print(p)
   })
+    
+    ##### Output percentage of control zeros
+    output$controlzeros <- renderText({ 
+      
+      # Get gene ID's reactively
+      geneID <- reactive({
+        # Input validation 
+        validate(
+          inputValidation(input$geneID)
+        )
+        input$geneID
+      })
+      
+      gene.df <- data.frame(gene=as.vector(as.numeric(data[rownames(data)==geneID(),])), group=groups)
+      
+      controlzeros <- length(which(gene.df[gene.df$group=="Control",]$gene==0)) / length(which(groups=="Control"))
+      controlzeros <- round(controlzeros*100,2)
+
+      paste("Control group: ", controlzeros, "%", sep="")
+    })
+    
+    ##### Output percentage of control zeros
+    output$kdzeros <- renderText({ 
+      
+      # Get gene ID's reactively
+      geneID <- reactive({
+        # Input validation 
+        validate(
+          inputValidation(input$geneID)
+        )
+        input$geneID
+      })
+      
+      gene.df <- data.frame(gene=as.vector(as.numeric(data[rownames(data)==geneID(),])), group=groups)
+      
+      kdzeros <- length(which(gene.df[gene.df$group=="Knockdown",]$gene==0)) / length(which(groups=="Knockdown"))
+      kdzeros <- round(kdzeros*100,2)
+      paste("Knockdown group: ", kdzeros, "%", sep="")
+    })
 })
--- ViolinPlots/ui.R
+++ ViolinPlots/ui.R
@@ -4,22 +4,29 @@ library("shiny")
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Violin Plot"),
+  titlePanel("Violin Plots of Gene Expression"),
   
   # Sidebar with a text box input for selecting genes
   sidebarLayout(
     
     sidebarPanel(
-      helpText("Enter Ensembl ID"),
-      textInput("geneID", label = h3("Ensembl ID"), 
-                value = "Enter text..."),
+      helpText("Violin plots depict expression of a given gene in Control vs. Knockdown cells. Note that the horizontal range of the points across the x-axis is simply a graphical jitter; it is meant for visual clarity and has no further meaning."),
+      textInput("geneID", label = h5("Enter Ensembl ID for gene of interest"), value = "e.g. ENSG..."),
       checkboxInput("log", "Plot gene expression on log scale", 
                     value = FALSE)
       ),
     
-    # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("violinPlot")
+      
+      # Show a plot of the generated distribution
+      plotOutput("violinPlot"),
+      br(),
+      br(),
+    
+      # Report percentage of zeros in either group
+      h4("Percentage of zero counts"),
+      textOutput("controlzeros"),
+      textOutput("kdzeros")
     )
   )
 ))
